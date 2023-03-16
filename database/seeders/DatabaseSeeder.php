f67a54cba7bb633f5c8e35f2fefbffa9033c1186
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2023-03-16 17:31:52 +0100

    Naprawiono moduły w Laravelu

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Database\Seeders\RoleSeeder;
 use Database\Seeders\UserSeeder;
 use Illuminate\Support\Facades\Hash;
+use Database\Seeders\HostingTypeSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,7 +23,12 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(PermissionSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(HostingTypeSeeder::class);
          \App\Models\User::factory(10)->create();
+        $this->call(HostingSeeder::class);
+        $this->call(TicketStatusSeeder::class);
+        $this->call(TicketSeeder::class);
+        $this->call(TicketMessageSeeder::class);
 
         //  \App\Models\User::factory()->create([
         //      'name' => 'Test User',
