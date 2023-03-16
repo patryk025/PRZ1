03b6f1e69cbc96491525a912e4bf1ff36b128a4e
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2023-03-16 14:26:42 +0100

    Wrzucono pliki z archiwum zip

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\InvoicesController;
+use App\Http\Controllers\InvoiceStatusesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::name('users.')->prefix('users')->group(function () {
+        Route::get('', [UserController::class, 'index'])
+            ->name('index')
+            ->middleware(['permission:users.index']);
+    });
+});
