d83597f63e495493fad8fb2c40c81ee21b75f3e0
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2023-03-30 19:12:27 +0200

    Added routing

--- routes/web.php
+++ routes/web.php
@@ -56,4 +56,10 @@ Route::middleware([
     Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
     Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 
+    Route::get('/courses', [CoursesController::class, 'index'])->name('courses.index');
+    Route::get('/courses/create', [CoursesController::class, 'create'])->name('courses.create');
+    Route::post('/courses', [CoursesController::class, 'store'])->name('courses.store');
+    Route::get('/courses/{id}/edit', [CoursesController::class, 'edit'])->name('courses.edit');
+    Route::put('/courses/{id}', [CoursesController::class, 'update'])->name('courses.update');
+    Route::delete('/courses/{id}', [CoursesController::class, 'destroy'])->name('courses.destroy');
 });
